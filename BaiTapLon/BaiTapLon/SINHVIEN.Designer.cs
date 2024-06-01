602458b4b8d9860f5316446029fe43fa891e52f4
hoanganhhieu123 <anhdinh1603.lol@gmail.com>
2024-06-01 20:45:32 +0700

    QuanLiSinhVien

---
+++ BaiTapLon/BaiTapLon/SINHVIEN.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace BaiTapLon
+{
+    partial class SINHVIEN
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbusername = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Datagridshow = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Datagridshow)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Tan;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lbusername);
+            this.panel1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1118, 98);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Tan;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(17, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Mã sinh viên:";
+            // 
+            // lbusername
+            // 
+            this.lbusername.AutoSize = true;
+            this.lbusername.BackColor = System.Drawing.Color.Tan;
+            this.lbusername.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbusername.Location = new System.Drawing.Point(104, 7);
+            this.lbusername.Name = "lbusername";
+            this.lbusername.Size = new System.Drawing.Size(55, 22);
+            this.lbusername.TabIndex = 1;
+            this.lbusername.Text = "         ";
+            this.lbusername.Click += new System.EventHandler(this.lbusername_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Controls.Add(this.Datagridshow);
+            this.panel2.Location = new System.Drawing.Point(13, 117);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1118, 570);
+            this.panel2.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 35);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1112, 50);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Datagridshow
+            // 
+            this.Datagridshow.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Datagridshow.Location = new System.Drawing.Point(3, 129);
+            this.Datagridshow.Name = "Datagridshow";
+            this.Datagridshow.RowHeadersWidth = 51;
+            this.Datagridshow.RowTemplate.Height = 24;
+            this.Datagridshow.Size = new System.Drawing.Size(1115, 441);
+            this.Datagridshow.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(426, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(195, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Thông tin sinh viên";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(435, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(177, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Điểm thi các môn";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // SINHVIEN
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(1154, 699);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "SINHVIEN";
+            this.Text = "SINHVIEN";
+            this.Load += new System.EventHandler(this.SINHVIEN_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Datagridshow)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView Datagridshow;
+        private System.Windows.Forms.Label lbusername;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ BaiTapLon/BaiTapLon/SINHVIEN.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace BaiTapLon
+{
+    public partial class SINHVIEN : Form
+    {
+        public SINHVIEN(string username)
+        {
+            InitializeComponent();
+            lbusername.Text = username;
+        }
+
+        #region
+        // cap nhat điểm từ co sở dữ liệu 
+        public void loadData()
+        {
+            string sql = "select TenMH, ChuyenCan,DieuKien,CuoiKi,KiHoc FROM  KETQUA WHERE  KETQUA.Masv = '" + lbusername.Text+"'";
+            Datagridshow.DataSource = KetNoi.getData(sql);
+        }
+        // cap nhat thong tin sinh vien tu co so du lieu
+        public void loadData2()
+        {
+            string sql = "SELECT SINHVIEN.Masv, SINHVIEN.HoTen, SINHVIEN.NgaySinh, SINHVIEN.GioiTinh, lOP.TenLop, KHOA.TenKhoa FROM SINHVIEN,LOP,KHOA WHERE SINHVIEN.MaLop = LOP.MaLop AND LOP.MaKhoa = KHOA.MaKhoa and SINHVIEN.Masv = '"+lbusername.Text+"'";
+            dataGridView1.DataSource = KetNoi.getData(sql);
+        }
+        #endregion  
+
+        private void SINHVIEN_Load(object sender, EventArgs e)
+        {
+            KetNoi.moketnoi();
+            loadData();
+
+            if (Datagridshow != null)
+            {
+                Datagridshow.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+                Datagridshow.Columns[Datagridshow.Columns.Count - 1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            }
+
+            KetNoi.moketnoi();
+            loadData2();
+            if (dataGridView1 != null)
+            {
+                dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+                dataGridView1.Columns[dataGridView1.Columns.Count - 1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbusername_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
